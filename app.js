2561bbf73642a6de9e7abe2371c376d933ded201
Ana Garcia <agarcialapole@gmail.com>
2021-07-16 09:38:26 +0200

    Añadido Comentarios

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
+// ! Esto es una prueba
+// * Prueba2
+// ? Prueba3
+// @param myParam el parametro de este metodo
+// TODO Hay que modificar
+
+// TODO:
+// FIXME:
+
+
 console.log("Hola mundo");
 console.log("Hola mundo2");
